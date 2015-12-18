996b801e8ffec9a6d7c8a28e435813ece6c90a87
Chuck Horton <chuckh@hware.com>
2015-12-18 12:00:00 -0800

    Move firebase.son to docs folder

--- travis-runner.sh
+++ travis-runner.sh
@@ -21,15 +21,17 @@ then
     echo " Making Changes to PSK for Firebase" && \
     sed -i.tmp 's/<!-- Chrome for Android theme color -->/<base href="\/">\'$'\n<!-- Chrome for Android theme color -->/g' app/index.html && \
     sed -i.tmp "s/hashbang: true/hashbang: false/" app/elements/routing.html && \
+    cp app/docs/firebase.json app/firebase.json && \
     echo " Starting Build Process for Firebase Changes" && \
     gulp && \
     echo " Starting Deploy Process to Firebaseapp.com Server -- polymer-starter-kit.firebaseapp.com" && \
-    firebase deploy -f polymer-starter-kit --non-interactive --token "${FIREBASE_TOKEN}" && \
+    firebase deploy --non-interactive --token "${FIREBASE_TOKEN}" && \
     echo " Undoing Changes to PSK for Firebase" && \
     cp app/index.html.tmp app/index.html && \
     cp app/elements/routing.html.tmp app/elements/routing.html && \
     rm app/elements/routing.html.tmp && \
     rm app/index.html.tmp && \
+    rm app/firebase.json && \
     echo " DONE deploying to Firebase!"
   }
 
